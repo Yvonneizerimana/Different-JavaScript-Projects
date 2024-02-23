654eb7e09bd18472636d24074556c02e693b985b
yvonne IZERIMANA <izerimanayvonne510@gmail.com>
2024-02-23 17:40:05 +0200

    to do again

--- todolist.js
+++ todolist.js
@@ -1,83 +1,113 @@
-// Define the ToDoList object
-const ToDoList = {
-    tasks: [],
-    categories: [],
-
-    // Function to add a category
-    addCategory: function(categoryName) {
-        const category = {
-            name: categoryName
-        };
-        this.categories.push(category);
-    },
-
-    // Function to add a task to the ToDo list with specified category
-    addTask: function(taskName, categoryName) {
-        const task = {
-            name: taskName,
-            category: categoryName,
-            completed: false
-        };
-        this.tasks.push(task);
-    },
-
-    // Function to display tasks grouped by their categories
-    displayTasksGroupedByCategory: function() {
-        // Group tasks by category
-        const groupedTasks = {};
-        this.tasks.forEach(task => {
-            if (!groupedTasks[task.category]) {
-                groupedTasks[task.category] = [];
-            }
-            groupedTasks[task.category].push(task);
-        });
-
-        // Display tasks grouped by category
-        for (const category of this.categories) {
-            const categoryName = category.name;
-            if (groupedTasks[categoryName]) {
-                console.log(`Category: ${categoryName}`);
-                groupedTasks[categoryName].forEach(task => {
-                    console.log(`- ${task.name} [${task.completed ? 'Completed' : 'Incomplete'}]`);
-                });
-            }
-        }
-    },
-
-    // Function to mark a task as completed
-    markTaskAsCompleted: function(taskName) {
-        const task = this.tasks.find(task => task.name === taskName);
-        if (task) {
-            task.completed = true;
-        }
-    },
-
-    // Function to remove a task from the ToDo list
-    removeTask: function(taskName) {
-        const index = this.tasks.findIndex(task => task.name === taskName);
-        if (index !== -1) {
-            this.tasks.splice(index, 1);
-        }
+// Task object
+const Task = {
+    createTask: function(name, completed = false) {
+      return {
+        name: name,
+        completed: completed
+      };
     }
-};
-
-// Example usage:
-ToDoList.addCategory('Work');
-ToDoList.addCategory('Personal');
-
-ToDoList.addTask('Complete project', 'Work');
-ToDoList.addTask('Go grocery shopping', 'Personal');
-ToDoList.addTask('Call mom', 'Personal');
-ToDoList.addTask('Prepare presentation', 'Work');
-
-console.log("--- Tasks grouped by category ---");
-ToDoList.displayTasksGroupedByCategory();
-
-console.log("\n--- Marking task 'Complete project' as completed ---");
-ToDoList.markTaskAsCompleted('Complete project');
-
-console.log("\n--- Removing task 'Call mom' ---");
-ToDoList.removeTask('Call mom');
-
-console.log("\n--- Updated tasks grouped by category ---");
-ToDoList.displayTasksGroupedByCategory();
+  };
+  
+  // Category object
+  const Category = {
+    createCategory: function(name) {
+      return {
+        name: name,
+        tasks: []
+      };
+    }
+  };
+  
+  // ToDoList object
+  const ToDoList = {
+    categories: [],
+  
+    addCategory: function(categoryName) {
+      const category = Category.createCategory(categoryName);
+      this.categories.push(category);
+      return category;
+    },
+  
+    addTask: function(taskName, categoryName) {
+      const category = this.categories.find(category => category.name === categoryName);
+      if (category) {
+        const task = Task.createTask(taskName);
+        category.tasks.push(task);
+        return task;
+      } else {
+        return { error: `Category '${categoryName}' not found.` };
+      }
+    },
+  
+    displayTasksGroupedByCategory: function() {
+      const groupedTasks = {};
+      this.categories.forEach(category => {
+        groupedTasks[category.name] = category.tasks.map(task => {
+          return {
+            name: task.name,
+            completed: task.completed
+          };
+        });
+      });
+      return groupedTasks;
+    },
+  
+    markTaskAsCompleted: function(taskName) {
+      let taskFound = false;
+      this.categories.forEach(category => {
+        const task = category.tasks.find(task => task.name === taskName);
+        if (task) {
+          task.completed = true;
+          taskFound = true;
+        }
+      });
+      if (!taskFound) {
+        return { error: `Task '${taskName}' not found.` };
+      } else {
+        return { message: `Task '${taskName}' marked as completed.` };
+      }
+    },
+  
+    removeTask: function(taskName) {
+      let taskRemoved = false;
+      this.categories.forEach(category => {
+        const taskIndex = category.tasks.findIndex(task => task.name === taskName);
+        if (taskIndex !== -1) {
+          category.tasks.splice(taskIndex, 1);
+          taskRemoved = true;
+        }
+      });
+      if (!taskRemoved) {
+        return { error: `Task '${taskName}' not found.` };
+      } else {
+        return { message: `Task '${taskName}' removed successfully.` };
+      }
+    }
+  };
+  
+  // Example usage
+  const category1 = ToDoList.addCategory('Work');
+  const category2 = ToDoList.addCategory('Personal');
+  
+  const task1 = ToDoList.addTask('Complete project', 'Work');
+  const task2 = ToDoList.addTask('Call mom', 'Personal');
+  const task3 = ToDoList.addTask('kuryama', 'Personal');
+  
+  const tasksGroupedByCategory = ToDoList.displayTasksGroupedByCategory();
+  
+  const markTaskResult = ToDoList.markTaskAsCompleted('Complete project');
+  const removeTaskResult = ToDoList.removeTask('kuryama');
+  
+  console.log("--- Tasks grouped by category ---");
+  console.log(tasksGroupedByCategory);
+  
+  console.log("--- Mark Task Result ---");
+  console.log(markTaskResult);
+  
+  console.log("--- Remove Task Result ---");
+  console.log(removeTaskResult);
+  
+  const updatedTasksGroupedByCategory = ToDoList.displayTasksGroupedByCategory();
+  
+  console.log("--- Updated Tasks grouped by category ---");
+  console.log(updatedTasksGroupedByCategory);
